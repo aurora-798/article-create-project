2b0d801b42a65190568d5daf0aed3ae663918a91
Attitude <3415181520@qq.com>
2026-05-19 20:55:38 +0800

    安装必要依赖

---
+++ src/main/java/com/shuhang/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.shuhang.controller;
+
+import com.shuhang.common.BaseResponse;
+import com.shuhang.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success("ok");
+    }
+}
+
