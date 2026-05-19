c1d01711ac74bd5a4acb85ba446db95283490315
Attitude <3415181520@qq.com>
2026-05-19 20:37:59 +0800

    后端项目初始化

---
+++ src/main/java/com/shuhang/ArticleCreateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shuhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleCreateProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleCreateProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shuhang/ArticleCreateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuhang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleCreateProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
